7318a01c163f9b0038e384962886092f05b5e5ab
xennia316 <xennia316@gmail.com>
2022-03-08 19:10:17 +0100

    intro to js

---
+++ Day15/JavaScript/script.js
@@ -0,0 +1,44 @@
+/**
+ * author: xennia316
+ */
+console.log("Sonia");
+console.log("Nice nice");
+
+// Variables are boxes that hold a piece of information. They're dynamic in nature and can change as demanded
+
+// var keyword
+var x, y, z;
+x = 1;
+y = 5;
+z = 23;
+console.log(z);
+
+// let keyword
+
+let name;
+name = "Ayah";
+console.log(name);
+
+// Reassigning already used variable 
+z = 21;
+console.log(z);
+
+// Const can only be used once.
+// const name;
+
+// JS operators
+z = 3 * (x + y) - y;
+console.log(z);
+
+// Concatenation of strings
+let me = "Nge"
+let you = "wung"
+let us = me + you;
+console.log(us);
+
+let random = 16;
+let random_square = random * random;
+console.log(random_square);
+// exponemtial in js
+random_square = random ** 2;
+console.log(random_square++);
