58d1d90adb2b8528902c1e3036882bbe16c58976
xennia316 <xennia316@gmail.com>
2022-03-09 19:24:54 +0100

    learning js concepts

---
+++ JavaScript/Day16/script.js
@@ -0,0 +1,40 @@
+/**
+ * by xennia316
+ */
+
+let firstName = "Amehmbo ";
+let secondName = " Ngewung";
+let name = firstName + secondName;
+console.log(name);
+
+// Arrays
+let classList = ["Sonia", "Vanessa", "Nyenti", 'Rachael', "Akinimbom", "Austin", "Carine", "Erica", "Edwin"];
+console.log(classList);
+
+console.log(classList.length);
+classList[0];
+
+// Objects
+let classDescription = {
+    boys: 3,
+    girls: 9,
+    teachers: 3,
+};
+console.log(classDescription.boys);
+console.log(classDescription["teachers"]);
+typeof 4;
+
+let age = 23;
+if (age >= 16) {
+    console.log("I am no longer a child and I am responsible for all my actions!");
+}
+else{
+    console.log("I am a kid and I trust my parents decisions for me")
+}
+
+// let nName = prompt("Enter a name");
+
+console.log(1 === "1");
+
+// Assignment
+let grade;
