8c52441426c98e1d825548c6c46993c6a2083408
xennia316 <xennia316@gmail.com>
2022-03-14 13:20:35 +0100

    file modified

---
+++ JavaScript/Day19/homework/work7.js
@@ -0,0 +1,16 @@
+/**
+ * by xennia316
+ */
+
+let num1, num2 , i;
+function comparator(num1, num2){
+    for (i == 100; i <= 200; i++) {
+        if (i == num1 || i == num2) {
+            return true;
+        }
+        else
+            return false;
+    }
+}
+
+console.log(comparator(150, 10));
