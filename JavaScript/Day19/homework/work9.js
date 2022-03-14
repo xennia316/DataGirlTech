6b00ce0e9470613cdfe8f9fdd021f8e79f8e7130
xennia316 <xennia316@gmail.com>
2022-03-14 13:26:34 +0100

    two inegers in a range

---
+++ JavaScript/Day19/homework/work9.js
@@ -0,0 +1,15 @@
+/**
+ * by xennia316
+ */
+ let num1, num2, num3;
+
+ function check_numbers( x , y){
+     if ((x >= 50 && x <= 99) || (y >= 50 && y <= 99)){
+         return true;
+     }
+     else {
+         return false;
+     }
+ }
+ 
+ console.log(check_numbers(120, 45));
