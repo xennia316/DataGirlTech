aef39480c60e8d1107d484ddc3cdf8907cae3279
xennia316 <xennia316@gmail.com>
2022-03-15 19:10:00 +0100

    checking repition of 15 in array

---
+++ JavaScript/Day19/homework/work75.js
@@ -0,0 +1,17 @@
+/**
+ * by xennia316
+ */
+
+let arr = [1, 2, 3, 4, 15, 15, 6, 2];
+
+let i= 0, j = 0;
+
+let checker = (values) => {
+    for (i = 1, j = 0; i < values.length; i++, j++) {
+        if (values[j] === values[i]) {
+            return true;
+        }
+        return false;
+    }
+}
+console.log(checker(arr));
