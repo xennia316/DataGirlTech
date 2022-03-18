6f63aba7b436b3ace0c762db4d5d2b23ae1698a1
xennia316 <xennia316@gmail.com>
2022-03-18 18:53:25 +0100

    DOM manipulation / JS file for calculator

---
+++ DOM Manipulation/Day22/calculator.js
@@ -0,0 +1,67 @@
+/**
+ * by xennia316
+ */
+
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator_keys');
+const display = document.querySelector('.calculator_display');
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType;
+
+
+        if (!action) {
+            if (displayedNum === '0' || previousKeyType === 'operator') {
+                display.textContent = keyContent
+            } else {
+                display.textContent = displayedNum + keyContent
+            }
+        }
+
+        if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+            key.classList.add('is-depressed');
+            calculator.dataset.previousKeyType = 'operator';
+            calculator.dataset.firstValue = displayedNum
+            calculator.dataset.operator = action
+        }
+        if (action === 'decimal') {
+            display.textContent = displayedNum + '.';
+        }
+
+        if (action === 'clear') {
+            display.textContent = 0;
+        }
+
+        if (action === 'calculate') {
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNum
+            const calculate = (n1, operator, n2) => {
+                let result = ''
+                
+                if (operator === 'add') {
+                  result = parseFloat(n1) + parseFloat(n2)
+                } else if (operator === 'subtract') {
+                  result = parseFloat(n1) - parseFloat(n2)
+                } else if (operator === 'multiply') {
+                  result = parseFloat(n1) * parseFloat(n2)
+                } else if (operator === 'divide') {
+                  result = parseFloat(n1) / parseFloat(n2)
+                }
+                
+                return result
+              }
+
+            display.textContent = calculate(firstValue, operator, secondValue)
+
+        }
+
+        Array.from(key.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'))
+    }
+})
