8a274b7269480624a84cfb77129aadc06b848f5d
xennia316 <xennia316@gmail.com>
2022-03-19 18:38:01 +0100

    looping through an array

--- JavaScript/Day19/homework/work75.js
+++ JavaScript/Day19/homework/work75.js
@@ -7,11 +7,14 @@ let arr = [1, 2, 3, 4, 15, 15, 6, 2];
 let i= 0, j = 0;
 
 let checker = (values) => {
-    for (i = 1, j = 0; i < values.length; i++, j++) {
-        if (values[j] === values[i]) {
-            return true;
+    if(values.length >= 2){
+        for (i = 1, j = 0; i < values.length; i++, j++) {
+            if (values[j] === values[i]) {
+                return true;
+            }
+            return false;
         }
-        return false;
     }
+    return false;
 }
 console.log(checker(arr));
